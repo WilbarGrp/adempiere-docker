7c7bd16591950463829f53d04226d94124f874e7
eelti <eriled@gmail.com>
2021-06-09 14:00:25 -0400

    add tool to restor db

---
+++ utils/restorToDb.ps1
@@ -0,0 +1,62 @@
+if (($args.count) -le 1){
+    Write-Output "********************You need to pass 2 Argument ... `n********** ex: restorToDb.ps1 From_Db To_Db"
+    exit 1
+}
+
+$FROM_DB=$args[0] #SAME AS adempiere tenant name
+$TO_DB=$args[1] #SAME AS adempiere tenant name
+
+$BASE_DIR=Get-Location
+
+
+$myFileName="db-bck-" + (Get-Date -UFormat "%d-%m-%y") + ".sql"
+
+$BACKUP_FILE="Thursday_LiveDbBackup.sql.gz"
+$BACKUP_FILE2=$BACKUP_FILE.Replace(".gz","")
+Write-Output "We will restor $BACKUP_FILE"
+$URL="https://fs.piscinetrendium.com:7443/$BACKUP_FILE"
+$USER_PSSWD="mike"
+Set-Clipboard "RoadBike401"
+
+if (Test-Path -Path $BASE_DIR){
+    if (Test-Path -LiteralPath $BACKUP_FILE){
+        Write-Output "Backup to restor already exist we will restor from that file $BACKUP_FILE"
+    }
+    else
+    { 
+      Write-Output "Download from $URL"
+       $t = $host.ui.RawUI.ForegroundColor
+       $host.ui.RawUI.ForegroundColor = "Red"
+       Write-Output "Ctrl+v to past the password"
+       $host.ui.RawUI.ForegroundColor = $t
+       Invoke-RestMethod -Uri $URL -OutFile $BACKUP_FILE -Credential mike
+       if (Test-Path -LiteralPath "$BACKUP_FILE"){
+        Write-Output "Db Backup download successful"
+        }
+        else
+        {
+                Write-Output "Db Backup not download"
+                    exit
+        }
+    }
+    & ${env:ProgramFiles}\7-Zip\7z.exe e $BACKUP_FILE -y > $null
+    Remove-Item $BACKUP_FILE
+ }
+
+Write-Output "backup actual db just in case ;-)"
+docker exec -t postgres132_db_1 pg_dump -U $TO_DB $TO_DB --no-owner > ./$myFileName
+
+docker stop $TO_DB
+docker stop postgres132_db_1
+docker start postgres132_db_1
+docker exec -it postgres132_db_1 dropdb -U $TO_DB $TO_DB
+docker exec -it postgres132_db_1 createdb -U $TO_DB -E unicode $TO_DB
+docker cp $BACKUP_FILE2 postgres132_db_1:/tmp
+docker exec postgres132_db_1 psql -U $TO_DB -d $TO_DB -f "/tmp/$BACKUP_FILE2"
+Write-Output "ALTER SCHEMA $FROM_DB RENAME TO $TO_DB;" | docker exec -i postgres132_db_1 psql -U $TO_DB 
+Write-Output "REASSIGN OWNED BY $FROM_DB TO $TO_DB;" | docker exec -i postgres132_db_1 psql -U @TO_DB 
+docker start $FROM_DB
+docker start $TO_DB
+Remove-Item $BACKUP_FILE2
+
+
---
+++ utils/restorToDb.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+set -x #echo on
+
+FROM_DB=$1 #SAME AS adempiere tenant name
+TO_DB=$2 #SAME AS adempiere tenant name
+
+BASE_DIR=$(cd "$(dirname "$0")"; pwd)
+
+myFileName=db-bck-$(date +%d-%m-%y).sql
+
+BACKUP_FILE="Thursday_LiveDbBackup.sql.gz"
+echo "We will restor $BACKUP_FILE"
+URL="https://fs.piscinetrendium.com:7443/"$BACKUP_FILE
+USER_PSSWD="mike:RoadBike401"
+if [ -d "$BASE_DIR" ]
+then
+    if [ -f "$BACKUP_FILE" ]
+    then
+       echo "Backup to restor already exist we will restor from that file $BACKUP_FILE"
+    else
+       echo "Download from $URL"
+       curl -u $USER_PSSWD -L $URL > $BACKUP_FILE
+       if [ -f "$BACKUP_FILE" ]
+       then
+         echo "Db Backup download successful"
+       else
+              "Db Backup not download"
+              exit
+       fi
+    fi
+fi
+
+echo "backup actual db just in case ;-)"
+docker exec -t postgres132_db_1 pg_dump -U $TO_DB $TO_DB --no-owner > ./$myFileName
+
+docker stop $TO_DB
+docker stop postgres132_db_1
+docker start postgres132_db_1
+docker exec -it postgres132_db_1 dropdb -U $TO_DB $TO_DB
+docker exec -it postgres132_db_1 createdb -U $TO_DB -E unicode $TO_DB
+
+docker exec -t postgres132_db_1 psql -U $TO_DB -d $TO_DB < "$BASE_DIR/$BACKUP_FILE"
+echo "ALTER SCHEMA $FROM_DB RENAME TO $TO_DB;" | docker exec -i postgres132_db_1 psql -U $TO_DB 
+echo "REASSIGN OWNED BY $FROM_DB TO $TO_DB;" | docker exec -i postgres132_db_1 psql -U @TO_DB 
+docker start $FROM_DB
+docker start $TO_DB
